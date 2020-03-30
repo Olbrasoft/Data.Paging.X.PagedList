8fe033fe5b58681780cdb54193d9dfe9f6387855
MatrixStore <matrixstore@email.cz>
2020-03-30 15:53:42 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Data.Paging.X.PagedList
+
+
+[![NuGet](https://img.shields.io/nuget/vpre/Olbrasoft.Data.Paging.X.PagedList.svg)](https://www.nuget.org/packages/Olbrasoft.Data.Paging.X.PagedList/)
+
 Extensions Olbrasoft.Data.Paging.IBasicPagedResult AsPagedList
+
+![Olbrasoft Dispatching](./olbrasoft-x-paged-list.png)
---
+++ src/Olbrasoft.Data.Paging.X.PagedList/SimplePagedList.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using X.PagedList;
+
+namespace Olbrasoft.Data.Paging.X.PagedList
+{
+    public class SimplePagedList<T> : BasePagedList<T>
+    {
+        public SimplePagedList(IEnumerable<T> subSet, int pageNumber, int pageSize, int totalItemCount) : base(pageNumber, pageSize, totalItemCount)
+        {
+            Subset.AddRange(subSet);
+        }
+    }
+}
